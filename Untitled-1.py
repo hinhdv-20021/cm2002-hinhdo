7594421d2afc2f33f04a709b2285fa3eb23cb856
hinhdv-2002 <130729013+hinhdv-2002@users.noreply.github.com>
2023-04-25 14:40:23 +0700

    First add

---
+++ Untitled-1.py
@@ -0,0 +1,13 @@
+
+# du lieu list
+x = [3, 2, 4, 5, 6, "a"]
+print(type(x))
+# du lieu set
+se_a= {2, 4, 5, "b"}
+print(type(se_a))
+#du lieu turple
+turl_b = ("a", 4, 9, 6, 7)
+print (type(turl_b))
+#du lieu dict
+dit_s = {"a" : "dilo", "b" : "gusd"}
+print(type(dit_s))
